8b7a0e4d993ff642db8025b9372af7af2d5ed4ca
alpha0212 <qkrcjfgml321@naver.com>
2022-07-13 00:55:25 +0900

    Updates

---
+++ precache-manifest.817c5b7e477a0e354ea136da02a9dd61.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "32c809a29b11bdeb73e76e1197e7703e",
+    "url": "/alpha0212/twitter/index.html"
+  },
+  {
+    "revision": "dbb0ed980d65cdfc3e3e",
+    "url": "/alpha0212/twitter/static/css/main.55bfdf87.chunk.css"
+  },
+  {
+    "revision": "56f2e1a838a4a251af69",
+    "url": "/alpha0212/twitter/static/js/2.5aa99344.chunk.js"
+  },
+  {
+    "revision": "34981cc0942697370fc9d635062a9d94",
+    "url": "/alpha0212/twitter/static/js/2.5aa99344.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dbb0ed980d65cdfc3e3e",
+    "url": "/alpha0212/twitter/static/js/main.4b112118.chunk.js"
+  },
+  {
+    "revision": "c429d067ed3adc814b85",
+    "url": "/alpha0212/twitter/static/js/runtime-main.8a720844.js"
+  }
+]);
